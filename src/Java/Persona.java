59ddce7024253e57217ee16b899653de60e1bd36
JoseER211 <88160386+JoseER211@users.noreply.github.com>
2022-03-17 22:43:10 -0500

    Creación de los objetos pedidos
    Se agregó el objeto persona, fruta y cuenta bancaria

---
+++ src/Java/Persona.java
@@ -0,0 +1,76 @@
+package Java;
+import java.util.Date;
+
+/**
+ *
+ */
+public class Persona {
+
+    private String name;
+    private String lastName1;
+    private String lastName2;
+    private Date dateBirth;
+    private float height;
+
+    public Persona(String name, String lastName1, String lastName2, Date dateBirth, float height) {
+        this.name = name;
+        this.lastName1 = lastName1;
+        this.lastName2 = lastName2;
+        this.dateBirth = dateBirth;
+        this.height = height;
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName1() {
+        return lastName1;
+    }
+
+    public void setLastName1(String lastName1) {
+        this.lastName1 = lastName1;
+    }
+
+    public String getLastName2() {
+        return lastName2;
+    }
+
+    public void setLastName2(String lastName2) {
+        this.lastName2 = lastName2;
+    }
+
+    public Date getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(Date dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "name='" + name + '\'' +
+                ", lastName1='" + lastName1 + '\'' +
+                ", lastName2='" + lastName2 + '\'' +
+                ", dateBirth=" + dateBirth +
+                ", height=" + height +
+                '}';
+    }
+}
+
